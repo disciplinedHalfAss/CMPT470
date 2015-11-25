35599e37634b21cac2791799df7909a97906f84e
alireza-a <alirezaa@sfu.ca>
2015-11-25 05:28:34 -0800

    adds the sort initial code

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 angular.module('mainApp', [])
-.controller('navbarCtrl', ['$scope', function ($scope) {
-	$scope.index;
-	$scope.setIndex = function(i){
-		$scope.index = i;
-	}	
-}])
 .factory('search',[function(){
   var o = {
     	universities: [
@@ -21,12 +15,6 @@ angular.module('mainApp', [])
                   { name: '300', id: 1 },
                   { name: '338', id: 2 }
                 ],	
-      sortOptions:  [
-                      { name: 'Price: low to high', id: 0 },
-                      { name: 'Price: high to low', id: 1 },
-                      { name: 'Avg. user review', id: 2 },
-                      { name: 'Newest postings', id: 3 }
-                    ]
   }
   return o;
 }])
@@ -38,7 +26,7 @@ angular.module('mainApp', [])
         id: 0, 
         name: "James Bond", 
         edition: '1st',
-        price: '$70',
+        price: '70',
         tags: ['asGoodAsNew'],
         ownersRating: 1, 
         numberOfReviews: 15
@@ -47,7 +35,7 @@ angular.module('mainApp', [])
         id: 1, 
         name: "Code Geass",
         edition: '3rd', 
-        price: '$88', 
+        price: '88', 
         tags: ['alright'],
         ownersRating: 3, 
         numberOfReviews: 10
@@ -56,7 +44,7 @@ angular.module('mainApp', [])
         id: 2, 
         name: "NginX",
         edition: '2nd', 
-        price: '$200', 
+        price: '200', 
         tags: ['myPrecious'],
         ownersRating: 5, 
         numberOfReviews: 100
@@ -65,18 +53,41 @@ angular.module('mainApp', [])
   }
   return o;
 }])
+.factory('selected', [function(){
+  var o = {
+    selectedUniversity: null,
+    selectedDepartment: null,
+    selectedCourse: null,
+    selectedSortOption: null,
 
-.controller('searchCtrl', ['$scope', 'search', function ($scope, search) {
+    sortOptions:  [
+                      { name: 'Price: low to high', id: 0 },
+                      { name: 'Price: high to low', id: 1 },
+                      { name: 'Avg. user review', id: 2 },
+                      { name: 'Newest postings', id: 3 }
+                    ]
+    
+  }
+  return o;
+}])
+.controller('navbarCtrl', ['$scope', function ($scope) {
+	$scope.index;
+	$scope.setIndex = function(i){
+		$scope.index = i;
+	}	
+}])
+.controller('searchCtrl', ['$scope', 'search', 'selected', function ($scope, search, selected) {
   
   $scope.universities = search.universities
   $scope.departments = search.departments
   $scope.courses = search.courses
-  $scope.sortOptions = search.sortOptions
+  
+  $scope.sortOptions = selected.sortOptions
 	
-	$scope.selectedUniversity;
-	$scope.selectedDepartment;
-	$scope.selectedCourse;
-	$scope.selectedSortOption;
+	$scope.selectedUniversity = selected.selectedUniversity;
+	$scope.selectedDepartment = selected.selectedDepartment;
+	$scope.selectedCourse = selected.selectedCourse;
+	$scope.selectedSortOption = selected.selectedSortOption;
 	
 	$scope.universityChanged = function(){
 
@@ -91,8 +102,26 @@ angular.module('mainApp', [])
 		
 	}
 }])
-.controller('booksIndexCtrl', ['$scope', 'books', function ($scope, books) {
-	$scope.books = books.books
+.controller('booksIndexCtrl', ['$scope', 'books', 'selected', function ($scope, books, selected) {
+	$scope.books = books.books;
+  $scope.books_order_by = function(book){
+    console.log(selected.selectedSortOption)
+    switch (selected.selectedSortOption){
+      case 0:
+        return book.price;
+        break;
+      case 1:
+        return -book.price;
+        break;
+      case 2:
+        return book.ownersRating;
+        break;
+      case 3:
+        break;
+      default:
+        return book.numberOfReviews;
+    } 
+  }
 	
 	$scope.getStars = function(n){
 		return new Array(n);
