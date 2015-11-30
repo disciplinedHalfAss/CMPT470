6f0543d7930a03d01c5cbcb4bc1b1b2cc22d94be
archits <archits@sfu.ca>
2015-11-29 17:05:53 -0800

    added functions to BooksController

--- chalupa/app/controllers/books_controller.rb
+++ chalupa/app/controllers/books_controller.rb
@@ -1,2 +1,42 @@
 class BooksController < ApplicationController
+
+
+	def index
+	    @books = Book.all
+	    respond_with @books
+	end
+
+	def show
+		respond_with Book.find(params[:id])
+	end
+
+	def new
+    	respond_with Book.new
+ 	end
+  
+  	def edit
+    	respond_with Book.find(params[:id])
+  	end
+
+	def create
+		Book.create(book_params)
+	end
+
+  	def update
+   		@book = Book.find(params[:id])
+    	@book.update(book_params)
+  	end
+ 
+  	def destroy
+    	@book = Book.find(params[:id])
+   	 	@book.destroy
+  	end
+
+
+private
+
+	def book_params
+		params.require(:book).permit(:course_id, :user_id, :isbn, :name, :author, :edition, :price, :tags, :description, :is_sold)
+	end
+
 end
