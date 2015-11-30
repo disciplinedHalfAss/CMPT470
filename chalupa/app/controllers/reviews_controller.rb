fcc029fa63a494f0e2c51b41b5dac6259bf3197f
archits <archits@sfu.ca>
2015-11-30 00:01:37 -0800

    Adds functionality to review_controller

--- chalupa/app/controllers/reviews_controller.rb
+++ chalupa/app/controllers/reviews_controller.rb
@@ -1,2 +1,46 @@
 class ReviewsController < ApplicationController
+
+	def index
+	    @reviews = Review.all
+      	render :json => @reviews
+	    #respond_with @reviews
+	end
+
+	def show
+		#respond_with Review.find(params[:id])
+    	render :json =>  Review.find(params[:id])
+	end
+
+	def new
+    	#respond_with Review.new
+      	render :json => Review.new
+ 	end
+  
+  	def edit
+	    #respond_with Review.find(params[:id])
+	    render :json => Review.find(params[:id])
+  	end
+
+	def create
+    	render :json => review_params
+		Review.create(review_params)
+	end
+
+  	def update
+    	@review = Review.find(params[:id])
+    	@review.update(review_params)
+  	end
+
+  	def destroy
+    	@review = Review.find(params[:id])
+    	@review.destroy
+  	end
+
+
+
+
+	def review_params
+		params.require(:review).permit(:made_by, :for, :description, :rating, :show)
+	end
+
 end
